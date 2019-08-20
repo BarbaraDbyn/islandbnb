935f0b037180138db0bda09c224ac7220b9e0c71
Jean Berti <berti.jean@gmail.com>
2019-08-20 18:21:41 +0200

    suppressed search bar, refacto cards, and css hover cards

---
+++ config/initializers/continent_card.rb
@@ -0,0 +1,26 @@
+CONTINENT_CARD = [
+  {
+    name: "Asie",
+    image_path: "https://images.unsplash.com/photo-1529333241880-9558d5e5e064?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Afrique",
+    image_path: "https://images.unsplash.com/photo-1436914228390-c080778dd589?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Amerique du Nord",
+    image_path: "https://images.unsplash.com/photo-1529333241880-9558d5e5e064?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Amerique du Sud",
+    image_path: "https://images.unsplash.com/photo-1518509562904-e7ef99cdcc86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Europe",
+    image_path: "https://images.unsplash.com/photo-1529333241880-9558d5e5e064?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Océanie",
+    image_path: "https://images.unsplash.com/photo-1443181994330-3e365ff8949e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  }
+]
