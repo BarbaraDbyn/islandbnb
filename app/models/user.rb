ac922943e8838f694f0cb470be8240dc17bc5d82
Jean Berti <berti.jean@gmail.com>
2019-08-21 15:25:51 +0200

    Active record issue & added island in seed.db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :islands, dependent: :destroy
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :city, presence: true
