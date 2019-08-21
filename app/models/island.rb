cb2adacf14a9b2867068626ac095c886a1e52056
Jean Berti <berti.jean@gmail.com>
2019-08-21 11:31:59 +0200

    #Improved neNEWw seed SEED.DB

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,6 +1,6 @@
 class Island < ApplicationRecord
   belongs_to :user
-  validates :continent, inclusion: { in: ['Asie', 'Amerique du Nord', 'Amerique du Sud', 'Europe', 'Afrique', 'Oceanie']}
+  validates :continent, inclusion: { in: ['Asie', 'Amerique du Nord', 'Amerique du Sud', 'Europe', 'Afrique', 'Océanie']}
   mount_uploader :photo, PhotoUploader
 end
 
