355e48f40f14babdd6b7aeb91073ed988b8896f8
Jean Berti <berti.jean@gmail.com>
2019-08-21 10:37:50 +0200

    #improved seed.DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,51 +6,86 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
-Island.destroy_all
 User.destroy_all
 
 puts 'creating user'
-jean = User.create(first_name: 'Jean', last_name: 'Berti', city: 'Lille', email: 'berti.jean@gmail.com', password: 'azerty')
+jean = User.create!(first_name: 'Jean', last_name: 'Berti', city: 'Lille', email: 'berti.jean@gmail.com', password: 'azerty')
+barbara = User.create!(first_name: 'Barbara', last_name: 'De Breyne', city: 'Comines', email: 'barbara.debreyne@orange.fr', password: 'azerty')
+yoann = User.create!(first_name: 'Yoann', last_name: 'Bettinelli', city: 'Luxembourg', email: 'interiste54@gmail.com', password: 'azerty')
 
+puts 'Finished creating Users'
 
-puts 'Creating an island...'
+Island.destroy_all
+puts 'Creating islands...'
 
   Island.create!(
-    name: 'Acapulco',
-    description: 'Elle est située sur le littoral bordant l\'Océan pacifique, à 400 km au sud de Mexico,
-                  dans une baie profonde semi-circulaire presque fermée, dont la facilité d\'accès et
-                  la sureté du mouillage permet aux bateaux de rester le long des rochers qui bordent la côte.
-                  La ville est construite sur une bande de terre étroite (moins d\'un kilomètre), entre la côte
-                  et les montagnes de la Sierra Madre del Sur qui encerclent la baie.
-                  Acapulco possède un climat tropical doux et le plus stable du continent américain, avec une
-                  température moyenne annuelle de 28 °C. Les ouragans peuvent éventuellement toucher la zone à la fin de l’été2.
-                  L\'effort pour introduire la brise de mer par un passage appelé l\'Abra de San Nicolas a eu des effets bénéfiques.
-                  Sa population d\'après le recensement de 2010 est de 673 479 habitants.',
-    continent: 'Asie',
+    name: 'Ilsa GitKommitzle ',
+    description: 'Peite île située sur le littoral bordant l\'Océan pacifique, bordée par et les montagnes de la Sierra Madre del Sur qui encerclent la baie.
+                  l\'Ilsa GitKommitzle possède un climat tropical doux et le plus stable du continent américain, avec une
+                  température moyenne annuelle de 28 °C, elle n\'attends que vous!',
+    continent: 'Amerique du Sud',
     availability: true,
-    price_per_day: 2000,
-    user: jean,
+    price_per_day: 12000,
+    user: barbara,
     photo: open('https://images.unsplash.com/photo-1505881502353-a1986add3762?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2762&q=80')
   )
 
   Island.create!(
-    name: 'Cancun',
-    description: 'blablabla',
+    name: 'Rails Island',
+    description: 'Rails Island, anciennement «Îles du Ruby», fait partie de l\'archipel canadien au large de la Colombie-Britannique.
+                  Situées dans les eaux côtières de l\'Alaska du Sud-Est et de la Colombie-Britannique, elles comprennent deux îles
+                  principales séparées par un très étroit chenal.',
     continent: 'Amerique du Nord',
     availability: true,
-    price_per_day: 3000,
+    price_per_day: 11000,
     user: jean,
     photo: open('https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?ixlib=rb-1.2.1&auto=format&fit=crop&w=2691&q=80')
   )
 
 Island.create!(
-    name: 'Madrid',
-    description: 'blablabla',
+    name: 'Pûsh ô Mâst',
+    description: 'Pûsh ô Mâst est une île de la province cambodgienne de JavaScrinkville. Elle est connue pour ses criques de sable et
+                  récifs coralliens, comme ceux situés autour du ponton de Koh Rong. À l\'intérieur des terres de l\'île,
+                  une jungle dense est parsemée de cocotiers et de cascades.',
     continent: 'Asie',
     availability: true,
-    price_per_day: 4000,
-    user: jean,
+    price_per_day: 13000,
+    user: yoann,
     photo: open('https://images.unsplash.com/photo-1468746587034-766ade47c1ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')
+  )
+
+Island.create!(
+    name: 'Sudo Teg',
+    description: 'Sudo Teg, toponyme tongien signifiant littéralement en français « grande Maraboue », est une des îles des Fidji.
+                  Elle concentre sur une superficie de 10 km², située à 16 kilomètres à l\'ouest de Viti Levu.',
+    continent: 'Océanie',
+    availability: true,
+    price_per_day: 40000,
+    user: yoann,
+    photo: open('https://images.unsplash.com/photo-1516091877740-fde016699f2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80')
+  )
+
+Island.create!(
+    name: 'Apété Guèt',
+    description: 'Apété Guèt fait partie de l\'archipel des Comores qui forme un ensemble d\'îles de l\'océan Indien situées au sud-est de l\'Afrique, entre le littoral nord mozambicain
+                  et à la pointe nord de Madagascar. Il est constitué principalement de quatre îles: Grande Comore, Anjouan, Mohéli et Mayotte,',
+    continent: 'Afrique',
+    availability: true,
+    price_per_day: 40000,
+    user: barbara,
+    photo: open('https://images.unsplash.com/photo-1530948990335-1eb93cbe6430?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
+  )
+
+Island.create!(
+    name: 'Haisse Cuèlle',
+    description: 'Haisse Cuèlle est une petite ile de la côte nord islandaise, située à 35 km au nord d\'Jïqrëyrïê dans l\'Eyjafjörður. L\'ile est desservie par
+                  un service de ferry partant de Árskógssandi et reliant l\'ile en 15 minutes. Depuis 2004, l\'ile n\'est plus une municipalité à part
+                  entière et fait partie de la municipalité d\'Jïqrëyrïê.',
+    continent: 'Europe',
+    availability: true,
+    price_per_day: 11000,
+    user: jean,
+    photo: open('https://images.unsplash.com/photo-1519491512949-b70aa1855e6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=796&q=80')
   )
 
 puts 'Finished!'
